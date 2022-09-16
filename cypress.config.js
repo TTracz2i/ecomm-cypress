ccbf8af61bcde597c4a8ad55bb08a61aa8f72de8
Tomasz Tracz <t@tracz.me>
2022-09-16 10:57:44 +0100

    Add cypress

---
+++ cypress.config.js
@@ -0,0 +1,47 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "cypress-mochawesome-reporter, mocha-junit-reporter",
+    cypressMochaawesomeReporterReporterOptions: {
+      reportDir: "cypress/reports",
+      charts: true,
+      reportPageTitle: "Steve's Report",
+      embeddedScreenshots: true,
+      inlineAssets: true,
+    },
+    mochaJunitReporterReporterOptions: {
+      mochaFile: "cypress/reports/junit/results-[hash].xml",
+    },
+  },
+
+  e2e: {
+    baseUrl: "https://www.edgewordstraining.co.uk/demo-site",
+    setupNodeEvents(on, config) {
+      const {
+        beforeRunHook,
+        afterRunHook,
+      } = require("cypress-mochawesome-reporter/lib");
+      // implement node event listeners here
+      const exec = require("child_process").execSync;
+
+      // implement node event listeners here
+      on("before:run", async (details) => {
+        console.log("override before:run");
+        await exec("rm -rf cypress/screenshots");
+
+        await exec("rm -rf cypress/reports");
+
+        await beforeRunHook(details);
+      });
+      on("after:run", async () => {
+        console.log("override after:run");
+        await exec(
+          "npx jrm ./cypress/reports/junitreport.xml ./cypress/reports/junit/*.xml"
+        );
+        await afterRunHook();
+      });
+    },
+  },
+});
