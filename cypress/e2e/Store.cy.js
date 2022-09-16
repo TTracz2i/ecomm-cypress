06b7922098ce247c90146f57eac90489e4e25a43
Tomasz Tracz <t@tracz.me>
2022-09-16 16:41:38 +0100

    Add screen sizes in pixels

--- cypress/e2e/Store.cy.js
+++ cypress/e2e/Store.cy.js
@@ -3,11 +3,16 @@ import { HomePage } from "./../page-objects/HomePage";
 describe("Test shopping functions", () => {
   const homePage = new HomePage();
 
-  const sizes = ["iphone-6", "ipad-2", "macbook-13", "macbook-16"];
+  const sizes = ["iphone-6", "ipad-2", "macbook-13", "macbook-16", [1024, 768]];
 
   sizes.forEach((size) => {
-    it(`Should work on ${size}`, () => {
-      cy.viewport(size);
+    it(`Should get discount on ${size}`, () => {
+      if (typeof size != "string") {
+        // if one of the values is null/undef/0 run default value
+        cy.viewport(size[0] || 1024, size[1] || 768);
+      } else {
+        cy.viewport(size);
+      }
       homePage.navigate("My account");
     });
   });
